f3daa7b1086016007820c19ee00abc24f67e9ab1
Ian-Woo Kim <ianwookim@gmail.com>
2022-05-05 11:26:26 -0700

    use nix flakes!

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "hgdal";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    fficxx = {
+      url = "github:wavewave/fficxx/0.6";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, fficxx }:
+    let
+      pkgs = import nixpkgs {
+        overlays = [ fficxx.overlay ];
+        system = "x86_64-linux";
+      };
+
+      finalHaskellOverlay = self: super:
+        (import ./default.nix {
+          inherit pkgs;
+          fficxxSrc = fficxx;
+        } self super);
+
+      newHaskellPackages = pkgs.haskellPackages.extend finalHaskellOverlay;
+
+    in {
+      packages.x86_64-linux = { inherit (newHaskellPackages) hgdal; };
+
+      # see these issues and discussions:
+      # - https://github.com/NixOS/nixpkgs/issues/16394
+      # - https://github.com/NixOS/nixpkgs/issues/25887
+      # - https://github.com/NixOS/nixpkgs/issues/26561
+      # - https://discourse.nixos.org/t/nix-haskell-development-2020/6170
+      overlay = final: prev: {
+        haskellPackages = prev.haskellPackages.override (old: {
+          overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
+            finalHaskellOverlay;
+        });
+      };
+
+      devShell.x86_64-linux = with pkgs;
+        let
+          hsenv = haskellPackages.ghcWithPackages (p: [
+            p.cabal-install
+            p.fficxx
+            p.fficxx-runtime
+            p.stdcxx
+            p.monad-loops
+          ]);
+        in mkShell {
+          buildInputs = [ hsenv gdal pkgconfig ];
+          shellHook = "";
+        };
+    };
+}
