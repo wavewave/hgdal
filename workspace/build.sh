bccf56fd795c6cbf822796eb427c2ead51f9fff6
Ian-Woo Kim <ianwookim@gmail.com>
2022-12-31 09:18:48 -0800

    a little more adjustment

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,5 +1,7 @@
+rm -rf hgdal
+rm -rf tmp
 cabal clean
-cabal exec -- ghc ../Gen.hs -package optparse-applicative
+ghc ../Gen.hs
 ../Gen gen
 cabal build hgdal
 cabal exec -- ghc example.hs  -package monad-loops  -lstdc++
